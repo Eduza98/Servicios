2f254a7324a03a57be34d838bb6fb28301c8aab2
Eduardo Z <62161803+Eduza98@users.noreply.github.com>
2022-09-11 13:34:24 -0500

    Delete servicio-usuarios directory

--- servicio-usuarios/src/main/java/edu/ucentral/serviciousuarios/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.ucentral.serviciousuarios.repository;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import edu.ucentral.commonusuario.model.Usuario;
-
-public interface UsuarioRepository extends PagingAndSortingRepository<Usuario, Long> {
-	public Usuario findByUsername(String username);
-}
--- servicio-usuarios/src/main/java/edu/ucentral/serviciousuarios/service/UsuarioService.java
+++
@@ -1,8 +0,0 @@
-package edu.ucentral.serviciousuarios.service;
-
-import edu.ucentral.commonsservicio.service.CommonService;
-import edu.ucentral.commonusuario.model.Usuario;
-
-public interface UsuarioService extends CommonService<Usuario> {
-	public Usuario findByUsername(String username);
-}
