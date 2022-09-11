d589f666cc9c91a07dce8fcec31e4d80bbdcfe38
jaime <jaime@Eduardo>
2022-09-11 13:36:19 -0500

    Servicio Zuul 11/09/2022

---
+++ servicio-zuul/src/test/java/edu/ucentral/servicio/zuul/ServicioZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ucentral.servicio.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
